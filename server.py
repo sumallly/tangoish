4c79dbeb514f3af4e141fabf9efa1c64f80938d4
sumallly <referlance@gmail.com>
2024-01-23 00:18:50 +0900

    Add func to judge str and return color

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify
+﻿from flask import Flask, request, render_template, jsonify
 import datetime, json, copy
 
 app = Flask(__name__, static_folder='.', static_url_path='')
@@ -22,5 +22,38 @@ def func():
     print(ret_dict)
     return json.dumps(ret_dict, ensure_ascii=False, indent=2)
 
+@app.route('/guess', methods=['POST'])
+def coloring():
+    # hit blow error
+    num_char = 5
+    right_str = 'しゃんぷー'
+    guess_str = 'じんじゃー' #request.form['guess_str']
+    right_list = list(right_str)
+    guess_list = list(guess_str)
+    judge = ['e' for i in range(num_char)]
+    judge_dict = {}
+    for i, char_g in enumerate(guess_list):
+        for j, char_r in enumerate(right_list):
+            if char_g == char_r:
+                judge[i] = 'h' if i == j else 'b'
+                break
+        if judge[i] == 'h':
+            judge_dict[i] = 'A7D28D'
+        elif judge[i] == 'b':
+            judge_dict[i] = 'FCC948'
+        else:
+            judge_dict[i] = '808080'
+            
+    print(guess_list)
+    print(right_list)
+    print(judge)
+    
+    ret_dict = {'match':int(all([u == 'h' for u in judge])),
+                'colors':judge_dict,
+               }
+    print(ret_dict)
+    return json.dumps(ret_dict, ensure_ascii=False, indent=4)
+
 if __name__ == '__main__':
-    app.run(port=8001, debug=True)
+    #app.run(port=8001, debug=True)
+    coloring();
