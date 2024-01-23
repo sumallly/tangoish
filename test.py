38cd71bf7e3491f5bea2da3c00c89842862baa08
sumallly <referlance@gmail.com>
2024-01-24 01:26:10 +0900

    memo, test

--- test.py
+++ test.py
@@ -11,11 +11,12 @@ print('----------------------------------------')
 for i in range(100):
     print(bytes.fromhex(hex(int('ぁ'.encode('utf-8').hex(), 16)+i)[2:]).decode('utf-8'))
 '''
+import json
 hrgn_n = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもや　ゆ　よらりるれろわをん　　'
 ktkn_n = 'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤ　ユ　ヨラリルレロワヲン　　'
 hrgn_s = 'ぁぃぅぇぉがぎぐげござじずぜぞだぢづでど　　っ　　ばびぶべぼぱぴぷぺぽゃ　ゅ　ょ　　　　　ー　　　　'
 ktkn_s = 'ァィゥェォガギグゲゴザジズゼゾダヂヅデド　　ッ　　バビブベボパピプペポャ　ュ　ョ　　　　　ー　　　　'
-
+'''
 hrgn_l = list(hrgn_n) + list(hrgn_s)
 ktkn_l = list(ktkn_n) + list(ktkn_s)
 base = {}
@@ -23,5 +24,17 @@ base = {}
 for i in range(len(hrgn_l)):
     base[hrgn_l[i]] = {'hrgn':hrgn_l[i], 'ktkn':ktkn_l[i], 'color':'00000000'}
 
-import json
-print(json.dumps(base, ensure_ascii=False))#, indent=4))
+print(json.dumps(base, ensure_ascii=False, indent=4))
+'''
+hrgn_n_l = list(hrgn_n)
+hrgn_s_l = list(hrgn_s)
+l = []
+for i in range(5):
+    l.append([])
+    for j in range(10):
+        l[i].append(hrgn_n_l[(9-j)*5+i])
+for i in range(5,10):
+    l.append([])
+    for j in range(10):
+        l[i].append(hrgn_s_l[(9-j)*5+i-5])
+print(json.dumps(l, ensure_ascii=False))
