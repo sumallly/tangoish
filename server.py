d040540d8af6342115a6b64647cf8ff25f36419c
sumallly <referlance@gmail.com>
2024-01-24 00:04:35 +0900

    add init_char_status_list

--- server.py
+++ server.py
@@ -9,6 +9,22 @@ def index():
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     return render_template('index.html', ip=user_ip, time=now)
 
+def char_basic_list():
+    hrgn_n = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもや　ゆ　よらりるれろわをん　　'
+    ktkn_n = 'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤ　ユ　ヨラリルレロワヲン　　'
+    hrgn_s = 'ぁぃぅぇぉがぎぐげござじずぜぞだぢづでど　　っ　　ばびぶべぼぱぴぷぺぽゃ　ゅ　ょ　　　　　ー　　　　'
+    ktkn_s = 'ァィゥェォガギグゲゴザジズゼゾダヂヅデド　　ッ　　バビブベボパピプペポャ　ュ　ョ　　　　　ー　　　　'
+
+    hrgn_l = list(hrgn_n) + list(hrgn_s)
+    ktkn_l = list(ktkn_n) + list(ktkn_s)
+    base = []
+    
+    for i in range(len(hrgn_l)):
+        base.append([hrgn_l[i], ktkn_l[i], '00000000'])
+        print(base[i])
+    
+    return base
+
 @app.route('/guess', methods=['POST'])
 def coloring():
     # hit blow error
@@ -25,11 +41,11 @@ def coloring():
                 judge[i] = 'h' if i == j else 'b'
                 break
         if judge[i] == 'h':
-            judge_dict[i] = 'A7D28D'
+            judge_dict[i] = 'A7D28DFF'
         elif judge[i] == 'b':
-            judge_dict[i] = 'FCC948'
+            judge_dict[i] = 'FCC948FF'
         else:
-            judge_dict[i] = '808080'
+            judge_dict[i] = '808080FF'
             
     print(guess_list)
     print(right_list)
