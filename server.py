baad025f4e0eb66430b35f2f9be929fa58c83730
sumallly <referlance@gmail.com>
2024-02-04 12:17:27 +0900

    change checkWord to fix from ktkn to hrgn

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import datetime, json
 
 requests = {}
 allowChars = list('あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんぁぃぅぇぉがぎぐげござじずぜぞだぢづでどっばびぶべぼぱぴぷぺぽゃゅょーアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンァィゥェォガギグゲゴザジズゼゾダヂヅデドッバビブベボパピプペポャュョー')
+hrgn = list('あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんぁぃぅぇぉがぎぐげござじずぜぞだぢづでどっばびぶべぼぱぴぷぺぽゃゅょー')
+ktkn = list('アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンァィゥェォガギグゲゴザジズゼゾダヂヅデドッバビブベボパピプペポャュョー')
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 
@@ -14,14 +16,19 @@ def index():
 
 @app.route('/check', methods=['POST'])
 def checkWord():
-    ret = 0
-    guess_word = request.form['word']
-    for u in list(guess_word):
-        if u not in allowChars:
-            ret += 1
+    eval_num = 0
+    guess_word = list(request.form['word'])
+    hrgn_word = guess_word
+    for i, char in enumerate(guess_word):
+        if char in hrgn:
+            continue
+        elif char in ktkn:
+            hrgn_word[i] = hrgn[ktkn.index(char)]
+        else:
+            eval_num += 1
             break
-    requests[str(request.remote_addr)] = guess_word
-    return str(ret)
+    requests[str(request.remote_addr)] = hrgn_word
+    return json.dumps({'eval':eval_num, 'hrgn':hrgn_word}, ensure_ascii=False)
 
 @app.route('/guess', methods=['POST'])
 def coloring():
@@ -31,29 +38,22 @@ def coloring():
     guess_word = requests.pop(str(request.remote_addr))
     right_list = list(right_word)
     guess_list = list(guess_word)
-    judge = ['e' for i in range(num_char)]
-    judge_dict = {}
+    judge = ['w' for i in range(num_char)]
     for i, char_g in enumerate(guess_list):
         for j, char_r in enumerate(right_list):
             if char_g == char_r:
                 judge[i] = 'h' if i == j else 'b'
                 break
-        if judge[i] == 'h':
-            judge_dict[char_g] = 0xA7D28DFF
-        elif judge[i] == 'b':
-            judge_dict[char_g] = 0xFCC948FF
-        else:
-            judge_dict[char_g] = 0x808080FF
             
     print(guess_list)
     print(right_list)
     print(judge)
     
     ret_dict = {'match':int(all([u == 'h' for u in judge])),
-                'colors':judge_dict,
+                'colors':judge,
                }
     print(ret_dict)
-    return json.dumps(ret_dict, ensure_ascii=False, indent=4)
+    return json.dumps(ret_dict)
 
 if __name__ == '__main__':
     app.run(port=8001, host='0.0.0.0', debug=True)
--- src/tangoish.js
+++ src/tangoish.js
@@ -10,8 +10,9 @@
     ["　", "　", "　", "ぺ", "べ", "　", "で", "ぜ", "げ", "ぇ"],
     ["　", "　", "ょ", "ぽ", "ぼ", "　", "ど", "ぞ", "ご", "ぉ"]];
 var sentWords = [];
-var charsJSON = { "あ": { "hrgn": "あ", "ktkn": "ア", "color": "h" }, "い": { "hrgn": "い", "ktkn": "イ", "color": "b" }, "う": { "hrgn": "う", "ktkn": "ウ", "color": "w" }, "え": { "hrgn": "え", "ktkn": "エ", "color": "y" }, "お": { "hrgn": "お", "ktkn": "オ", "color": "y" }, "か": { "hrgn": "か", "ktkn": "カ", "color": "y" }, "き": { "hrgn": "き", "ktkn": "キ", "color": "y" }, "く": { "hrgn": "く", "ktkn": "ク", "color": "y" }, "け": { "hrgn": "け", "ktkn": "ケ", "color": "y" }, "こ": { "hrgn": "こ", "ktkn": "コ", "color": "y" }, "さ": { "hrgn": "さ", "ktkn": "サ", "color": "y" }, "し": { "hrgn": "し", "ktkn": "シ", "color": "y" }, "す": { "hrgn": "す", "ktkn": "ス", "color": "y" }, "せ": { "hrgn": "せ", "ktkn": "セ", "color": "y" }, "そ": { "hrgn": "そ", "ktkn": "ソ", "color": "y" }, "た": { "hrgn": "た", "ktkn": "タ", "color": "y" }, "ち": { "hrgn": "ち", "ktkn": "チ", "color": "y" }, "つ": { "hrgn": "つ", "ktkn": "ツ", "color": "y" }, "て": { "hrgn": "て", "ktkn": "テ", "color": "y" }, "と": { "hrgn": "と", "ktkn": "ト", "color": "y" }, "な": { "hrgn": "な", "ktkn": "ナ", "color": "y" }, "に": { "hrgn": "に", "ktkn": "ニ", "color": "y" }, "ぬ": { "hrgn": "ぬ", "ktkn": "ヌ", "color": "y" }, "ね": { "hrgn": "ね", "ktkn": "ネ", "color": "y" }, "の": { "hrgn": "の", "ktkn": "ノ", "color": "y" }, "は": { "hrgn": "は", "ktkn": "ハ", "color": "y" }, "ひ": { "hrgn": "ひ", "ktkn": "ヒ", "color": "y" }, "ふ": { "hrgn": "ふ", "ktkn": "フ", "color": "y" }, "へ": { "hrgn": "へ", "ktkn": "ヘ", "color": "y" }, "ほ": { "hrgn": "ほ", "ktkn": "ホ", "color": "y" }, "ま": { "hrgn": "ま", "ktkn": "マ", "color": "y" }, "み": { "hrgn": "み", "ktkn": "ミ", "color": "y" }, "む": { "hrgn": "む", "ktkn": "ム", "color": "y" }, "め": { "hrgn": "め", "ktkn": "メ", "color": "y" }, "も": { "hrgn": "も", "ktkn": "モ", "color": "y" }, "や": { "hrgn": "や", "ktkn": "ヤ", "color": "y" }, "　": { "hrgn": "　", "ktkn": "　", "color": "y" }, "ゆ": { "hrgn": "ゆ", "ktkn": "ユ", "color": "y" }, "よ": { "hrgn": "よ", "ktkn": "ヨ", "color": "y" }, "ら": { "hrgn": "ら", "ktkn": "ラ", "color": "y" }, "り": { "hrgn": "り", "ktkn": "リ", "color": "y" }, "る": { "hrgn": "る", "ktkn": "ル", "color": "y" }, "れ": { "hrgn": "れ", "ktkn": "レ", "color": "y" }, "ろ": { "hrgn": "ろ", "ktkn": "ロ", "color": "y" }, "わ": { "hrgn": "わ", "ktkn": "ワ", "color": "y" }, "を": { "hrgn": "を", "ktkn": "ヲ", "color": "y" }, "ん": { "hrgn": "ん", "ktkn": "ン", "color": "y" }, "ぁ": { "hrgn": "ぁ", "ktkn": "ァ", "color": "y" }, "ぃ": { "hrgn": "ぃ", "ktkn": "ィ", "color": "y" }, "ぅ": { "hrgn": "ぅ", "ktkn": "ゥ", "color": "y" }, "ぇ": { "hrgn": "ぇ", "ktkn": "ェ", "color": "y" }, "ぉ": { "hrgn": "ぉ", "ktkn": "ォ", "color": "y" }, "が": { "hrgn": "が", "ktkn": "ガ", "color": "y" }, "ぎ": { "hrgn": "ぎ", "ktkn": "ギ", "color": "y" }, "ぐ": { "hrgn": "ぐ", "ktkn": "グ", "color": "y" }, "げ": { "hrgn": "げ", "ktkn": "ゲ", "color": "y" }, "ご": { "hrgn": "ご", "ktkn": "ゴ", "color": "y" }, "ざ": { "hrgn": "ざ", "ktkn": "ザ", "color": "y" }, "じ": { "hrgn": "じ", "ktkn": "ジ", "color": "y" }, "ず": { "hrgn": "ず", "ktkn": "ズ", "color": "y" }, "ぜ": { "hrgn": "ぜ", "ktkn": "ゼ", "color": "y" }, "ぞ": { "hrgn": "ぞ", "ktkn": "ゾ", "color": "y" }, "だ": { "hrgn": "だ", "ktkn": "ダ", "color": "y" }, "ぢ": { "hrgn": "ぢ", "ktkn": "ヂ", "color": "y" }, "づ": { "hrgn": "づ", "ktkn": "ヅ", "color": "y" }, "で": { "hrgn": "で", "ktkn": "デ", "color": "y" }, "ど": { "hrgn": "ど", "ktkn": "ド", "color": "y" }, "っ": { "hrgn": "っ", "ktkn": "ッ", "color": "y" }, "ば": { "hrgn": "ば", "ktkn": "バ", "color": "y" }, "び": { "hrgn": "び", "ktkn": "ビ", "color": "y" }, "ぶ": { "hrgn": "ぶ", "ktkn": "ブ", "color": "y" }, "べ": { "hrgn": "べ", "ktkn": "ベ", "color": "y" }, "ぼ": { "hrgn": "ぼ", "ktkn": "ボ", "color": "y" }, "ぱ": { "hrgn": "ぱ", "ktkn": "パ", "color": "y" }, "ぴ": { "hrgn": "ぴ", "ktkn": "ピ", "color": "y" }, "ぷ": { "hrgn": "ぷ", "ktkn": "プ", "color": "y" }, "ぺ": { "hrgn": "ぺ", "ktkn": "ペ", "color": "y" }, "ぽ": { "hrgn": "ぽ", "ktkn": "ポ", "color": "y" }, "ゃ": { "hrgn": "ゃ", "ktkn": "ャ", "color": "y" }, "ゅ": { "hrgn": "ゅ", "ktkn": "ュ", "color": "y" }, "ょ": { "hrgn": "ょ", "ktkn": "ョ", "color": "y" }, "ー": { "hrgn": "ー", "ktkn": "ー", "color": "y" } };
+var charsJSON = { "あ": { "hrgn": "あ", "ktkn": "ア", "color": "y" }, "い": { "hrgn": "い", "ktkn": "イ", "color": "y" }, "う": { "hrgn": "う", "ktkn": "ウ", "color": "y" }, "え": { "hrgn": "え", "ktkn": "エ", "color": "y" }, "お": { "hrgn": "お", "ktkn": "オ", "color": "y" }, "か": { "hrgn": "か", "ktkn": "カ", "color": "y" }, "き": { "hrgn": "き", "ktkn": "キ", "color": "y" }, "く": { "hrgn": "く", "ktkn": "ク", "color": "y" }, "け": { "hrgn": "け", "ktkn": "ケ", "color": "y" }, "こ": { "hrgn": "こ", "ktkn": "コ", "color": "y" }, "さ": { "hrgn": "さ", "ktkn": "サ", "color": "y" }, "し": { "hrgn": "し", "ktkn": "シ", "color": "y" }, "す": { "hrgn": "す", "ktkn": "ス", "color": "y" }, "せ": { "hrgn": "せ", "ktkn": "セ", "color": "y" }, "そ": { "hrgn": "そ", "ktkn": "ソ", "color": "y" }, "た": { "hrgn": "た", "ktkn": "タ", "color": "y" }, "ち": { "hrgn": "ち", "ktkn": "チ", "color": "y" }, "つ": { "hrgn": "つ", "ktkn": "ツ", "color": "y" }, "て": { "hrgn": "て", "ktkn": "テ", "color": "y" }, "と": { "hrgn": "と", "ktkn": "ト", "color": "y" }, "な": { "hrgn": "な", "ktkn": "ナ", "color": "y" }, "に": { "hrgn": "に", "ktkn": "ニ", "color": "y" }, "ぬ": { "hrgn": "ぬ", "ktkn": "ヌ", "color": "y" }, "ね": { "hrgn": "ね", "ktkn": "ネ", "color": "y" }, "の": { "hrgn": "の", "ktkn": "ノ", "color": "y" }, "は": { "hrgn": "は", "ktkn": "ハ", "color": "y" }, "ひ": { "hrgn": "ひ", "ktkn": "ヒ", "color": "y" }, "ふ": { "hrgn": "ふ", "ktkn": "フ", "color": "y" }, "へ": { "hrgn": "へ", "ktkn": "ヘ", "color": "y" }, "ほ": { "hrgn": "ほ", "ktkn": "ホ", "color": "y" }, "ま": { "hrgn": "ま", "ktkn": "マ", "color": "y" }, "み": { "hrgn": "み", "ktkn": "ミ", "color": "y" }, "む": { "hrgn": "む", "ktkn": "ム", "color": "y" }, "め": { "hrgn": "め", "ktkn": "メ", "color": "y" }, "も": { "hrgn": "も", "ktkn": "モ", "color": "y" }, "や": { "hrgn": "や", "ktkn": "ヤ", "color": "y" }, "　": { "hrgn": "　", "ktkn": "　", "color": "y" }, "ゆ": { "hrgn": "ゆ", "ktkn": "ユ", "color": "y" }, "よ": { "hrgn": "よ", "ktkn": "ヨ", "color": "y" }, "ら": { "hrgn": "ら", "ktkn": "ラ", "color": "y" }, "り": { "hrgn": "り", "ktkn": "リ", "color": "y" }, "る": { "hrgn": "る", "ktkn": "ル", "color": "y" }, "れ": { "hrgn": "れ", "ktkn": "レ", "color": "y" }, "ろ": { "hrgn": "ろ", "ktkn": "ロ", "color": "y" }, "わ": { "hrgn": "わ", "ktkn": "ワ", "color": "y" }, "を": { "hrgn": "を", "ktkn": "ヲ", "color": "y" }, "ん": { "hrgn": "ん", "ktkn": "ン", "color": "y" }, "ぁ": { "hrgn": "ぁ", "ktkn": "ァ", "color": "y" }, "ぃ": { "hrgn": "ぃ", "ktkn": "ィ", "color": "y" }, "ぅ": { "hrgn": "ぅ", "ktkn": "ゥ", "color": "y" }, "ぇ": { "hrgn": "ぇ", "ktkn": "ェ", "color": "y" }, "ぉ": { "hrgn": "ぉ", "ktkn": "ォ", "color": "y" }, "が": { "hrgn": "が", "ktkn": "ガ", "color": "y" }, "ぎ": { "hrgn": "ぎ", "ktkn": "ギ", "color": "y" }, "ぐ": { "hrgn": "ぐ", "ktkn": "グ", "color": "y" }, "げ": { "hrgn": "げ", "ktkn": "ゲ", "color": "y" }, "ご": { "hrgn": "ご", "ktkn": "ゴ", "color": "y" }, "ざ": { "hrgn": "ざ", "ktkn": "ザ", "color": "y" }, "じ": { "hrgn": "じ", "ktkn": "ジ", "color": "y" }, "ず": { "hrgn": "ず", "ktkn": "ズ", "color": "y" }, "ぜ": { "hrgn": "ぜ", "ktkn": "ゼ", "color": "y" }, "ぞ": { "hrgn": "ぞ", "ktkn": "ゾ", "color": "y" }, "だ": { "hrgn": "だ", "ktkn": "ダ", "color": "y" }, "ぢ": { "hrgn": "ぢ", "ktkn": "ヂ", "color": "y" }, "づ": { "hrgn": "づ", "ktkn": "ヅ", "color": "y" }, "で": { "hrgn": "で", "ktkn": "デ", "color": "y" }, "ど": { "hrgn": "ど", "ktkn": "ド", "color": "y" }, "っ": { "hrgn": "っ", "ktkn": "ッ", "color": "y" }, "ば": { "hrgn": "ば", "ktkn": "バ", "color": "y" }, "び": { "hrgn": "び", "ktkn": "ビ", "color": "y" }, "ぶ": { "hrgn": "ぶ", "ktkn": "ブ", "color": "y" }, "べ": { "hrgn": "べ", "ktkn": "ベ", "color": "y" }, "ぼ": { "hrgn": "ぼ", "ktkn": "ボ", "color": "y" }, "ぱ": { "hrgn": "ぱ", "ktkn": "パ", "color": "y" }, "ぴ": { "hrgn": "ぴ", "ktkn": "ピ", "color": "y" }, "ぷ": { "hrgn": "ぷ", "ktkn": "プ", "color": "y" }, "ぺ": { "hrgn": "ぺ", "ktkn": "ペ", "color": "y" }, "ぽ": { "hrgn": "ぽ", "ktkn": "ポ", "color": "y" }, "ゃ": { "hrgn": "ゃ", "ktkn": "ャ", "color": "y" }, "ゅ": { "hrgn": "ゅ", "ktkn": "ュ", "color": "y" }, "ょ": { "hrgn": "ょ", "ktkn": "ョ", "color": "y" }, "ー": { "hrgn": "ー", "ktkn": "ー", "color": "y" } };
 var flags = {};
+var alertText = '';
 
 function time() {
     document.getElementById("t1").innerHTML = new Date().toLocaleString();
@@ -26,18 +27,23 @@ function checkWord(inputData) {
             data: { 'word': inputData }
         }).done(function (res) {
             $('#response').html(res);
+            var resJSON = JSON.parse(res);
+            evalNum = resJSON['eval'];
             var alertText = '';
-            if (res == 0)
-                resolve(true);
+            console.log(typeof (evalNum));
+            if (evalNum == 0) {
+                console.log('eval 0');
+                resolve(resJSON['hrgn']);
+            }
             else {
-                if (res & 1)
+                alertText = '';
+                if (evalNum & 1)
                     alertText += 'キーボードにある文字のみを入力してください\n';
-                if (res & 2)
+                if (evalNum & 2)
                     alertText += 'test2\n';
-                if (res & 4)
+                if (evalNum & 4)
                     alertText += 'test4\n';
-                alert(alertText);
-                resolve(false);
+                resolve(null);
             }
         }).fail(function () {
             alert('error!!! (/checkWord)');
@@ -45,36 +51,43 @@ function checkWord(inputData) {
         });
     });
 }
-function reflectColor(colors) {
+
+function reflectColor(guessWord, colors) {
     return new Promise((resolve) => {
-        console.log(colors);
-        for (var char in colors) {
-            charsJSON[char]['color'] = colors[char]
-        }
-        console.log(charsJSON);
+        guessWord.forEach(function (char, i) {
+            charsJSON[char]['color'] = colors[i];
+            var key = document.getElementById(char)
+            key.classList.replace('jg_y', 'jg_' + charsJSON[char]['color']);
+            key.classList.replace('jg_w', 'jg_' + charsJSON[char]['color']);
+            key.classList.replace('jg_b', 'jg_' + charsJSON[char]['color']);
+        })
         resolve(colors);
     })
 }
 
 async function submit() {
-    const inputData = $('#input_data').val();
-    var valid = await checkWord(inputData);
-    if (valid) {
-        sentWords.unshift(inputData)
-        document.getElementById("input").innerHTML = sentWords;
-        $.ajax({
-            type: 'POST',
-            url: '/guess',
-        }).done(function (res) {
-            resJSON = JSON.parse(res);
-            // else not match
-            console.log(resJSON)
-            reflectColor(resJSON['colors']);
-            $('#result').html(res);
-        }).fail(function () {
-            alert('error!!!');
-        });
+    const guessWord = $('#input_data').val();
+    var hrgn = await checkWord(guessWord);
+    var resJSON;
+    console.log('hrgn',hrgn);
+    if (!hrgn) {
+        console.log('resolve null');
+        alert(alertText);
+        return 0;
     }
+    sentWords.unshift(guessWord)
+    document.getElementById("input").innerHTML = sentWords;
+    await $.ajax({
+        type: 'POST',
+        url: '/guess',
+    }).done(function (res) {
+        resJSON = JSON.parse(res);
+        $('#result').html(res);
+    }).fail(function () {
+        alert('error!!!');
+    });
+    // else not match
+    ret = await reflectColor(hrgn, resJSON['colors']);
 }
 
 window.addEventListener('load', () => {
@@ -90,10 +103,9 @@ window.addEventListener('load', () => {
             key.textContent = charsJSON[char]['hrgn'];
             key.classList.add('key_bt');
             key.classList.add('jg_' + charsJSON[char]['color']);
-            console.log(key);
+            key.id = char;
             keyboard.appendChild(key);
         }
         keyboard.appendChild(document.createElement('br'));
     }
-    console.log(charsJSON)
 })
